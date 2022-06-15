dab8a41bf9ae2124a7b7cbda48a7508d4e693a9c
ryu <ryu000407@gmail.com>
2022-06-15 19:41:36 +0900

    update: add form data deserialize

--- src/main.rs
+++ src/main.rs
@@ -5,5 +5,7 @@ use app::run;
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
     let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port");
+    let port = listener.local_addr().unwrap().port();
+    println!("http://127.0.0.1:{port}");
     run(listener)?.await
 }
