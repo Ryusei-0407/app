b7ac5b6baae7f15ba141877fe01f283ef799436a
ryu <ryu000407@gmail.com>
2022-06-16 03:59:38 +0900

    fix: name columns

--- migrations/20220615170938_create_subscriptions_table.sql
+++ migrations/20220615170938_create_subscriptions_table.sql
@@ -2,6 +2,6 @@ CREATE TABLE subscriptions (
     id uuid NOT NULL,
     PRIMARY KEY (id),
     email TEXT NOT NULL UNIQUE,
-    naem TEXT NOT NULL,
+    name TEXT NOT NULL,
     subscribed_at timestamptz NOT NULL
 );
