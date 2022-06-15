a5662b5d89463475b7e8be49a2d4045d55f166de
ryu <ryu000407@gmail.com>
2022-06-16 02:36:59 +0900

    update: update database scripts

--- scripts/init.sh
+++ scripts/init.sh
@@ -3,12 +3,28 @@
 set -x
 set -eo pipefail
 
+if ! [ -x "$(command -v sqlx)" ]; then
+    echo >&2 "Error: sqlx is not installed."
+    echo >&2 "Use:"
+    echo >&2 "      cargo install sqlx-cli --features postgres"
+    echo >&2 "to install it."
+    exit 1
+fi
+
 DB_USER=${POSTGRES_USER:=postgres}
 DB_PASSWORD="${POSTGRES_PASSWORD:=password}"
 DB_NAME="${POSTGRES_DB:=newsletter}"
 DB_PORT="${POSTGRES_POST:=5432}"
 
-docker-compose up -d --build
+if [[ -z "${SKIP_DOCKER}" ]]
+then
+    docker-compose up -d --build
+fi
 
-export DATABASE_URL=postgres://${DB_USER}:${DB_PASSWORD}@localhost:${DB_PORT}/${DB_NAME}
+echo >&2 "Postgres is up and running on port ${DB_PORT} - running migrations now!"
+
+export $(cat .env | xargs)
 sqlx database create
+sqlx migrate run
+
+echo >&2 "Postgres has been migrated."
