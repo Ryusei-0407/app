0107608ec11f6f04551a899091d82767410ce584
ryu <ryu000407@gmail.com>
2022-06-16 14:29:15 +0900

    update: add subscriptions end point process

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,16 @@
+use app::configuration::get_configuration;
+use app::startup::run;
+use sqlx::PgPool;
 use std::net::TcpListener;
-
-use app::run;
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port");
-    let port = listener.local_addr().unwrap().port();
-    println!("http://127.0.0.1:{port}");
-    run(listener)?.await
+    let configuration = get_configuration().expect("Failed to read configuration.");
+    let connection = PgPool::connect(&configuration.database.connections_string())
+        .await
+        .expect("Failed to connect to Postgres.");
+    let address = format!("127.0.0.1:{}", configuration.application_port);
+    let listener = TcpListener::bind(address)?;
+
+    run(listener, connection)?.await
 }
