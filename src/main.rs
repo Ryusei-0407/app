d9285e3087f71b07d377ce78fcb12c573d6b72cc
ryu <ryu000407@gmail.com>
2022-06-21 08:48:24 +0900

    update: add logger middleware

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,12 @@
 use app::configuration::get_configuration;
 use app::startup::run;
+use env_logger::Env;
 use sqlx::PgPool;
 use std::net::TcpListener;
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
+    env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
     let configuration = get_configuration().expect("Failed to read configuration.");
     let connection = PgPool::connect(&configuration.database.connections_string())
         .await
