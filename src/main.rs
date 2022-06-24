dc80137456e6881b62288d98a1c2fc9109a21cac
ryu <ryu000407@gmail.com>
2022-06-24 23:29:38 +0900

    refactor: main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,22 @@
-use app::configuration::get_configuration;
 use app::startup::run;
-use sqlx::PgPool;
+use app::telemetry::init_subscriber;
+use app::{configuration::get_configuration, telemetry::get_subscriber};
+use sqlx::postgres::PgPool;
 use std::net::TcpListener;
-use tracing::subscriber::set_global_default;
-use tracing_bunyan_formatter::{BunyanFormattingLayer, JsonStorageLayer};
-use tracing_log::LogTracer;
-use tracing_subscriber::{layer::SubscriberExt, EnvFilter, Registry};
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    LogTracer::init().expect("Failed to set logger");
-
-    let env_filter = EnvFilter::try_from_default_env().unwrap_or_else(|_| EnvFilter::new("info"));
-    let formatting_layer = BunyanFormattingLayer::new("app".into(), std::io::stdout);
-    let subscriber = Registry::default()
-        .with(env_filter)
-        .with(JsonStorageLayer)
-        .with(formatting_layer);
-
-    set_global_default(subscriber).expect("Failed to set subscriber");
+    let subscriber = get_subscriber("app".into(), "info".into());
+    init_subscriber(subscriber);
 
     let configuration = get_configuration().expect("Failed to read configuration.");
-    let connection = PgPool::connect(&configuration.database.connections_string())
+    let connection_pool = PgPool::connect(&configuration.database.connections_string())
         .await
         .expect("Failed to connect to Postgres.");
     let address = format!("127.0.0.1:{}", configuration.application_port);
     let listener = TcpListener::bind(address)?;
 
-    run(listener, connection)?.await
+    run(listener, connection_pool)?.await?;
+
+    Ok(())
 }
