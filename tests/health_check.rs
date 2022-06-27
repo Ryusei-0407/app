3d362bfaef9f9f63a4e5a39e8011ea51514a67c1
ryu <ryu000407@gmail.com>
2022-06-27 15:53:41 +0900

    test: fix database connection

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -2,6 +2,7 @@ use app::configuration::{get_configuration, DatabaseSettings};
 use app::startup::run;
 use app::telemetry::{get_subscriber, init_subscriber};
 use once_cell::sync::Lazy;
+use secrecy::ExposeSecret;
 use sqlx::{Connection, Executor, PgConnection, PgPool};
 use std::net::TcpListener;
 use uuid::Uuid;
@@ -46,15 +47,16 @@ async fn spawn_app() -> TestApp {
 }
 
 pub async fn configure_database(config: &DatabaseSettings) -> PgPool {
-    let mut connection = PgConnection::connect(&config.connection_string_without_db())
-        .await
-        .expect("Failed to connect to Postgres");
+    let mut connection =
+        PgConnection::connect(&config.connection_string_without_db().expose_secret())
+            .await
+            .expect("Failed to connect to Postgres");
     connection
         .execute(format!(r#"CREATE DATABASE "{}";"#, config.database_name).as_str())
         .await
         .expect("Failed to create database");
 
-    let connection_pool = PgPool::connect(&config.connections_string())
+    let connection_pool = PgPool::connect(&config.connections_string().expose_secret())
         .await
         .expect("Failed to connect to Postgres.");
     sqlx::migrate!("./migrations")
