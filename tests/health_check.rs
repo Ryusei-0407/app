8439aba248002ddfb0ae996fa62371b827a3f767
ryu <ryu000407@gmail.com>
2022-06-30 12:31:32 +0900

    fix: remove unused crate

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -2,7 +2,6 @@ use app::configuration::{get_configuration, DatabaseSettings};
 use app::startup::run;
 use app::telemetry::{get_subscriber, init_subscriber};
 use once_cell::sync::Lazy;
-use secrecy::ExposeSecret;
 use sqlx::{Connection, Executor, PgConnection, PgPool};
 use std::net::TcpListener;
 use uuid::Uuid;
